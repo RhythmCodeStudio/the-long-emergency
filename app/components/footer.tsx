0385ca2326c89876d8655099914858b789ec99a3
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-05-13 09:11:54 -0500

    continue setup

---
+++ app/components/footer.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 flex w-full  items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black  ">
+    <a
+      className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+      href="https://rhythmcodestudio.tech"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Image
+        src="/images/rcs-logo.png"
+        alt="Vercel Logo"
+        className="dark:invert w-24 h-auto"
+        width={796}
+        height={816}
+        priority
+      />
+    </a>
+  </footer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,24 @@
 "use client";
- 
+
 import { UploadButton } from "./utils/uploadthing";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-  <header className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex absolute top-0">
-    <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-      The Long Emergency
-    </p>
-  </header>
-
-  <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-    <Image
-      className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-      src="/images/rcs-logo.png"
-      alt="RCS Logo"
-      width={180}
-      height={37}
-      priority
-    />
-  </div>
-
-  <UploadButton
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert w-52 h-auto"
+          src="/images/rcs-logo.png"
+          alt="RCS Logo"
+          width={796}
+          height={816}
+          priority
+        />
+        
+      </div>
+      <UploadButton
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
           // Do something with the response
@@ -35,28 +30,6 @@ export default function Home() {
           alert(`ERROR! ${error.message}`);
         }}
       />
-
-
-
-
-
-  <footer className="fixed bottom-0 left-0 flex  w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black  ">
-      <a
-        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src="/images/rcs-logo.png"
-          alt="Vercel Logo"
-          className="dark:invert"
-          width={100}
-          height={24}
-          priority
-        />
-      </a>
-    </footer>
-</main>
+    </main>
   );
 }

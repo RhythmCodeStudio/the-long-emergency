8c61af55ede953d58b7c727880fc3ead31dcc6eb
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-05-20 12:29:43 -0500

    edit z index on header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import Nav from "./nav";
 
 export const Header = () => {
   return (
-    <header >
+    <header className="z-10">
       <div className="flex flex-col items-center w-full justify-center pb-8 pt-12 ">
         <span className=" text-2xl ">The Long Emergency</span>
         <Nav />

1d07aedf4e7f31b1af8818a0a54279ffaf8a051c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-06-06 07:59:38 -0500

    remove quotes

--- app/components/band-bio.tsx
+++ app/components/band-bio.tsx
@@ -3,7 +3,7 @@ export default function BandBio() {
     <div className="px-8 py-4">
       {/* <h3 className="text-lg lg:text-3xl text-center">The Long Emergency</h3> */}
       <p className="font-mono 2xl:text-xl p-2">
-        The Long Emergency is led by multi-instrumentalist and songwriter Kevin Long. From 2003 to 2013 Kevin was a mainstay on the Twin Cities independent music scene, performing as a solo artist and with bands including Sorry OK, The Drug Budget, and Bouncer Fighter. In 2013 Kevin formed The Long Emergency as a vehicle for his songwriting. The band released its debut album \"Starting Over\" in May of that year.
+        The Long Emergency is led by multi-instrumentalist and songwriter Kevin Long. From 2003 to 2013 Kevin was a mainstay on the Twin Cities independent music scene, performing as a solo artist and with bands including Sorry OK, The Drug Budget, and Bouncer Fighter. In 2013 Kevin formed The Long Emergency as a vehicle for his songwriting. The band released its debut album Starting Over in May of that year.
       </p>
     </div>
   )

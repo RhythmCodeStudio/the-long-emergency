64ec77ccf859b1f005a98542867cc2d45f19d1ea
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-06-16 11:18:16 -0500

    fix contact form env vars in code

--- app/components/contact-form.tsx
+++ app/components/contact-form.tsx
@@ -112,10 +112,10 @@ export default function ContactForm() {
       try {
         emailjs
           .send(
-            process.env.EMAILJS_SERVICE_ID || '',
-            process.env.EMAILJS_TEMPLATE_ID || '',
+            process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || '',
+            process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || '',
             emailTemplateParams,
-            process.env.EMAILJS_USER_ID
+            process.env.NEXT_PUBLIC_EMAILJS_USER_ID
           )
           .then((result) => {
             track("Contact form submission");

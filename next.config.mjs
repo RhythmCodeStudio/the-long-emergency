ef6bc281ae0eb96f3c77daf18a6a4a440cc33528
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-06-23 09:36:51 -0500

    lclhost with anchor

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+// next.config.mjs
+
+import autoCert from "anchor-pki/auto-cert/integrations/next";
+
+const withAutoCert = autoCert({
+  enabledEnv: "development",
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withAutoCert(nextConfig);

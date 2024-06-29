96440f2fb609d68af6282212ba18d7a7e40ddb3d
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-06-29 14:52:53 -0500

    switch

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
 // import from emailjs
-import emailjs from "@emailjs/browser";
+// import emailjs from "@emailjs/browser";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
@@ -18,31 +18,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
       sig!,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
-    console.log("event", event.type);
+    console.log("event", event?.type);
     console.log("res", res);
-    // Handle the event
-    const customerEmail = res?.data?.object?.billing_details?.email;
-    console.log("customerEmail", customerEmail);
-    
-    const emailFilesToCustomer = (email: string) => {
-      try {
-        emailjs.send(
-          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || '',
-            process.env.NEXT_PUBLIC_EMAILJS_I35_PURCHASE|| '',
-            {} // Fix: Pass an empty object as the last argument
-        )
-      } catch (error) {
-        console.log("error", error);
-      }
-    }
-
-    if (event.type === "checkout.session.completed") {
-      console.log("checkout.session.completed");
-      // Fulfill the purchase...
-      // Send email to customer
-      emailFilesToCustomer(customerEmail);
-    }
-
 
     return NextResponse.json({ status: "Success", event: event.type });
   } catch (error) {

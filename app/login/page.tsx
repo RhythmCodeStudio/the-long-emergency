a0dd16833d455b577fecf0b0d909f9cc47b123fd
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-12-09 08:38:04 -0600

    restore and remove

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+// import data
+import { getPage } from "../lib/data";
+// import components
+import LoginForm from "../../components/login-form";
+
+
+export default async function LoginPage() {
+  const loginPageData = await getPage("login");
+  return (
+    <div>
+      <h2>{loginPageData.page_title}</h2>
+      <div className="mt-24">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}

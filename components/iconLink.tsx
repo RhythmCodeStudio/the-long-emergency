82b95bc481c640c45795b5039eec740b7e1fe424
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-09-03 08:32:13 -0500

    update icon link

--- components/iconLink.tsx
+++ components/iconLink.tsx
@@ -1,8 +1,9 @@
 "use client";
 // import from vercel
 import { track } from "@vercel/analytics";
+import React from "react";
 
-export default function IconLink({ href, icon, label, name }: { href: string, icon: JSX.Element, label: string, name: string }) {
+export default function IconLink({ href, icon, label, name }: { href: string, icon: React.ReactElement, label: string, name: string }) {
   return (
     <a href={href} target="_blank" rel="noopener noreferrer" aria-label={label} onClick={() => {
       track(`${name} clicked`);

17a88437a53115d5d36708fbe9c070c44de28314
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-06-23 22:30:23 -0500

    update signup modal

--- ui/mailinglist-signup-modal.tsx
+++ ui/mailinglist-signup-modal.tsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 // import components
 import MailingListSignupForm from "./mailing-list-sign-up-form";
 import Button from "./button";
-import StarrySky from "./starry-sky";
 // import from react icons
 import { FiXCircle } from "react-icons/fi";
 
@@ -18,7 +17,7 @@ export default function MailingListSignupModal() {
         label="Sign Up for Mailing List"
         title="Sign up for po's mailing list"
         onClick={() => setIsOpen(true)}
-        className="cursor-pointer border-2 border-border-default p-1 px-4 rounded-full rainbow-gradient text-shadow-black-background-black w-full font-semibold shadow-md shadow-white hover:shadow-lg transform transition-transform transition-shadow duration-200 active:scale-95"
+        className="cursor-pointer border-2 border-border-default p-1 px-4 rounded-full rainbow-gradient text-shadow-black-background-black w-full font-semibold shadow-md shadow-white hover:shadow-lg transform transition-transform duration-200 active:scale-95"
       />
       {isOpen && (
         <div className="fixed inset-0 rainbow-gradient flex items-center justify-center z-50">
@@ -31,7 +30,6 @@ export default function MailingListSignupModal() {
             />
             <MailingListSignupForm />
           </div>
-          <StarrySky />
         </div>
       )}
     </>

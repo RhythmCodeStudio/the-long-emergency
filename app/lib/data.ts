e267a39c7957936907203c48444c2794aa2ecb50
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-06-24 20:04:25 -0500

    save

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,7 +3,7 @@ import postgres from 'postgres';
 import { User, Section, Page, TextBlock, Image, Album, Song, MerchProduct } from "@/definitions/definitions";
 import { unstable_noStore as noStore } from 'next/cache';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 
 export async function getAlbum(id: number) {
   try {

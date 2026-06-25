bc753cadaf4112ef42b77bb0bffad7fac14a8282
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-06-24 19:42:41 -0500

    update

--- ui/header.tsx
+++ ui/header.tsx
@@ -1,48 +1,6 @@
-// "use client";
-// //import components
-// import Nav from "./nav";
-// // import ScrollingBanner from "./scrolling-banner";
-// //import from next
-// import Link from "next/link";
-// import { usePathname } from "next/navigation";
-
-// export const Header = () => {
-//   const currentPath = usePathname();
-//   return (
-//     <header
-//       // className={`p-4 ${currentPath!=="/" ? "bg-[url('/images/background-images/masks-no-text-4800x3190-gaps-filled-horizontal-25per.png')] bg-no-repeat bg-cover bg-center xl:bg-none" : " "}`}
-//       className={`p-4 ${currentPath !== "/" ? "bg-[rgb(0,0,0,0.5)] bg-no-repeat bg-cover bg-center md:bg-transparent" : " "}`}>
-//       <div className="flex flex-col items-center w-full justify-center">
-//         <h1
-//           className={`
-//         font-emergency text-outline expand-on-load text-4xl xl:text-5xl 2xl:text-6xl m-2 p-2 
-//         ${currentPath === "/" ? "hidden" : "hidden sm:flex"}
-//       `}>
-//           <Link href="/">The Long Emergency</Link>
-//         </h1>
-//         <div className="flex items-center w-full">
-//           <h1
-//             id="main-heading-most-pages"
-//             className={`font-emergency px-2 text-outline expand-on-load text-xl xs:text-2xl   ${
-//               currentPath === "/" ? "hidden" : "sm:hidden"
-//             }`}>
-//             <Link href="/">The Long Emergency</Link>
-//           </h1>
-//           <div className="sm:flex sm:justify-center sm:w-full">
-//             <Nav />
-//           </div>
-//         </div>
-//       </div>
-//       <div>{/* <ScrollingBanner /> */}</div>
-//     </header>
-//   );
-// };
-
-
 "use client";
 //import components
 import Nav from "./nav";
-// import ScrollingBanner from "./scrolling-banner";
 //import from next
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -75,8 +33,7 @@ export const Header = ({ isAuthenticated }: HeaderProps) => {
         currentPath !== "/"
           ? "bg-[rgb(0,0,0,0.5)] bg-no-repeat bg-cover bg-center md:bg-transparent"
           : " "
-      }`}
-    >
+      }`}>
       <div className="flex flex-col items-center w-full justify-center">
         {currentPath !== "/" && isSmUp === true && (
           <h1 className="font-emergency text-outline expand-on-load text-4xl xl:text-5xl 2xl:text-6xl m-2 p-2">
@@ -88,8 +45,7 @@ export const Header = ({ isAuthenticated }: HeaderProps) => {
           {currentPath !== "/" && isSmUp === false && (
             <h1
               id="main-heading-most-pages"
-              className="font-emergency px-2 text-outline expand-on-load text-xl xs:text-2xl"
-            >
+              className="font-emergency px-2 text-outline expand-on-load text-xl xs:text-2xl">
               <Link href="/">The Long Emergency</Link>
             </h1>
           )}

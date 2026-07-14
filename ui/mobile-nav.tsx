bd404912d377b4b34058622c58efedd5c7f7bbe2
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-07-14 14:08:39 -0500

    refactor mobile nav

--- ui/mobile-nav.tsx
+++ ui/mobile-nav.tsx
@@ -10,6 +10,7 @@ import { FiMenu } from "react-icons/fi";
 // import components
 // import SignOutButton from "./sign-out-button";
 import MorePopover from "./more-popover";
+import NavListItem from "./nav-list-item";
 
 type NavItem = {
   label: string;
@@ -45,49 +46,57 @@ export default function MobileNav({
         <div className="flex flex-col fixed inset-0 bg-black z-50 justify-center items-center h-full min-h-screen bg-cover bg-center bg-no-repeat bg-[url('/images/background-images/masks-no-text-4800x3190-gaps-filled.png')]">
           <div className="flex justify-end">
             <button
-              className="top-4 right-4 absolute"
+              className="top-4 right-4 absolute z-50"
               onClick={closeMenu}
               aria-label="Close Menu">
               <FiX className="text-2xl" />
             </button>
           </div>
-          <nav id="mobile-nav-link-container" className="font-bold relative">
+          <nav id="mobile-nav-link-container" className="font-bold flex flex-col items-center justify-center bg-zinc-950/60 fixed inset-0">
             <Link href="/">
               <h1
-                className="font-emergency text-center text-2xl m-6"
+                className="font-emergency text-center text-2xl pb-6"
                 onClick={closeMenu}>
                 The Long Emergency
               </h1>
             </Link>
-            <ul className="text-xl">
-              {navListItems.map((link) => (
-                <li
-                  key={link.label}
-                  className={`font-bold text-center w-full py-6 ${
-                    pathname === link.href ? "hidden" : ""
-                  }`}>
-                  {link.label === "Support" ? (
-                    <a
-                      href={link.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="flex flex-col items-center justify-center"
-                      onClick={closeMenu}>
-                      <span>{link.label}</span>
-                    </a>
-                  ) : (
-                    <Link href={link.href}>
-                      <div
-                        className="flex flex-col items-center justify-center"
-                        onClick={closeMenu}>
-                        <span>{link.label}</span>
-                      </div>
-                    </Link>
-                  )}
-                </li>
+            <ul className="text-xl max-w-30 mx-auto">
+              {navListItems.map((item) => (
+                // <li
+                //   key={link.label}
+                //   className={`font-bold text-center w-full py-6 ${
+                //     pathname === link.href ? "hidden" : ""
+                //   }`}>
+                //   {link.label === "Support" ? (
+                //     <a
+                //       href={link.href}
+                //       target="_blank"
+                //       rel="noopener noreferrer"
+                //       className="flex flex-col items-center justify-center"
+                //       onClick={closeMenu}>
+                //       <span>{link.label}</span>
+                //     </a>
+                //   ) : (
+                //     <Link href={link.href}>
+                //       <div
+                //         className="flex flex-col items-center justify-center"
+                //         onClick={closeMenu}>
+                //         <span>{link.label}</span>
+                //       </div>
+                //     </Link>
+                //   )}
+                // </li>
+                <NavListItem
+                  key={item.label}
+                  label={item.label}
+                  href={item.href}
+                  htmlElement={item.htmlElement}
+                  onClick={closeMenu}
+                  className="py-4"
+                />
               ))}
 
-              <li className="py-6">
+              <li className="flex justify-center">
                 <MorePopover 
                   isAuthenticated={isAuthenticated}
                   onAnyAction={closeMenu} anchor="top" />
